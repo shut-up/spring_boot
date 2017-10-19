3ca5d2f4f768d5fdfd4d315ad9b58a20a73c2d59
shut-up <936097245@qq.com>
2017-10-19 10:59:12 +0800

    在yml文件中配置属性并在controler中获取

--- src/main/java/com/example/demo/helloWorldController.java
+++ src/main/java/com/example/demo/helloWorldController.java
@@ -10,11 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class helloWorldController {
 
+    //获取yml配置文件中的属性
     @Value("${cupSize}")
     private String cupSize;
 
+    @Value("${age}")
+    private Integer age;
+
     @RequestMapping(value = "/hello" ,method = RequestMethod.GET)
     public  String say(){
-        return "hello spring boot!" + cupSize;
+        return "hello spring boot!" + cupSize + age;
     }
 }
